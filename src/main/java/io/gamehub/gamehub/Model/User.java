47a6b4b4f3bebaa37d55eebebd3e0b0988407402
th <t.hadj11010@gmail.com>
2025-04-09 19:57:32 +0100

    feat: add User model

---
+++ src/main/java/io/gamehub/gamehub/Model/User.java
@@ -0,0 +1,19 @@
+package io.gamehub.gamehub.Model;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    private ObjectId id;
+    private ObjectId username;
+}
