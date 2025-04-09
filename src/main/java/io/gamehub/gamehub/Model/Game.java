e0c9e2fd9b870415beac0e34931f06336ca7baea
th <t.hadj11010@gmail.com>
2025-04-09 20:02:08 +0100

    feat: add Game Model

---
+++ src/main/java/io/gamehub/gamehub/Model/Game.java
@@ -0,0 +1,27 @@
+package io.gamehub.gamehub.Model;
+
+import java.util.List;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "games")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Game {
+    @Id
+    private ObjectId id;
+    private String title;
+    private String description;
+    private List<String> systemRequirements;
+    private double price;
+    @DocumentReference
+    private User author;
+}
