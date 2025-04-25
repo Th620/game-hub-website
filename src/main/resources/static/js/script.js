d2de1b55ea36e8b7d9421f4e81457efb46e33d31
th <t.hadj11010@gmail.com>
2025-04-25 18:28:59 +0100

    feat :add styling for login pge

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,4 +1,4 @@
-document.getElementById("loginForm").addEventListener("submit", async (e) => {
+document.getElementById("authForm").addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const email = document.getElementById("email").value;
